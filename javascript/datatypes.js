d01dcbff8a9d7ecf077f2148f2adcddf0a1204a5
HunnursidhaGode <hsgodehg123@gmail.com>
2025-11-25 20:52:04 +0530

    completed premitive datatype

---
+++ javascript/datatypes.js
@@ -0,0 +1,22 @@
+//  primitive datatypes in javascript
+
+//1 string 
+var myname="Hunnursidha";
+console.log(myname);
+// 1 number
+var myage=35;
+console.log(myage);
+// 3 Boolean
+var Hunnursidha
+console.log("Hunnursidha");
+// 4 Boolean true or false 
+var isRaining=true;
+var completeddeiiner=false;
+console.log(isRaining);
+console.log(completeddeiiner)
+// 5 null
+var ok=null;
+console.log(ok)
+// 6 big int
+var bigint=1213244444414;
+console.log(bigint)
--- javascript/valuesandvaribles.js
+++ javascript/valuesandvaribles.js
@@ -1,4 +1,4 @@
 var myname  ="Hunnursidha";
 
-
+console.log(myname)
 console.log("Wel come to javascript programming");
