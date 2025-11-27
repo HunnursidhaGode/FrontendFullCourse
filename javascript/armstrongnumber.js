fd36b6bc1fa17cacd404082550caaf3474297821
HunnursidhaGode <hsgodehg123@gmail.com>
2025-11-27 06:37:14 +0530

    armstrong number

---
+++ javascript/armstrongnumber.js
@@ -0,0 +1,14 @@
+const prompt = require("prompt-sync")();
+let num = Number(prompt("enter the number"));
+let sum = 0;
+let temp = num;
+while (num > 0) {
+  let r = num % 10;
+  sum = sum + r * r * r;
+  num = Math.floor(num / 10);
+}
+if (sum == temp) {
+  console.log("number is armstrong");
+} else {
+  console.log("number is not armstrong");
+}
