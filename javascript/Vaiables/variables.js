17f5269afc56ce57b9309df7b179de32c4cd5254
HunnursidhaGode <hsgodehg123@gmail.com>
2025-12-29 23:01:03 +0530

    understand the variable

---
+++ javascript/Vaiables/variables.js
@@ -0,0 +1,4 @@
+// variables are the containers in which we can store data values
+// In JavaScript, we can declare variables using var, let, or const keywords
+console.log("This is the variables.js file.");
+console.log(10 + 10);
